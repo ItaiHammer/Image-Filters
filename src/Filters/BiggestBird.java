be364449d0ac7566a8543dc846936760989c115c
Itai Hammer <itaihammer1@gmail.com>
2023-01-12 22:12:01 -0800

    border

---
+++ src/Filters/BiggestBird.java
@@ -0,0 +1,24 @@
+package Filters;
+
+import Interfaces.PixelFilter;
+import core.DImage;
+
+public class BiggestBird implements PixelFilter {
+
+    public BiggestBird() {
+        
+    }
+
+    public DImage processImage (DImage img) {
+        short[][] grid = img.getBWPixelGrid();
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                
+            }
+        }
+
+        img.setPixels(grid);
+        return img;
+    }
+}
--- src/Filters/Border.java
+++ src/Filters/Border.java
@@ -12,11 +12,15 @@ public class Border implements PixelFilter {
 
     public DImage processImage (DImage img) {
         short[][] BWGrid = img.getBWPixelGrid();
-        short[][] grid = new short[BWGrid[0].length][BWGrid.length];
+        short[][] grid = new short[BWGrid[0].length + (thickness*2)][BWGrid.length + (thickness*2)];
 
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[r].length; c++) {
-                if () {}
+                if (c <= thickness || c >= thickness + BWGrid[0].length || r <= thickness || r >= thickness + BWGrid.length) {
+                    grid[r][c] = 0;
+                }else {
+                    grid[r][c] = BWGrid[r-thickness][c-thickness];
+                }
             }
         }
 
