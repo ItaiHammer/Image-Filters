c1fc46521ac881bd541cf627dd562c2477f9aa9e
Itai Hammer <itaihammer1@gmail.com>
2023-01-23 11:44:46 -0800

    merge

--- src/Filters/BetterDownsampling.java
+++ src/Filters/BetterDownsampling.java
@@ -10,15 +10,46 @@ public class BetterDownsampling implements PixelFilter {
     }
 
     public DImage processImage (DImage img) {
+        short[][] ogGrid = img.getBWPixelGrid();
         short[][] grid = img.getBWPixelGrid();
+        short[][] finalGrid;
+
+        System.out.println(grid[0].length);
+
+        while (grid[0].length % 4 != 0) {
+            grid = new short[grid.length][grid[0].length-1];
+        }
+        
+        System.out.println(grid[0].length);
+        System.out.println(grid[0].length % 4);
+
+        System.out.println("------");
+
+        System.out.println(grid.length);
+
+        while (grid.length % 4 != 0) {
+            grid = new short[grid.length - 1][grid[0].length];
+        }
+
+        System.out.println(grid.length);
+        System.out.println(grid.length % 4);
 
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[r].length; c++) {
-                
+                grid[r][c] = ogGrid[r][c];
             }
         }
 
-        img.setPixels(grid);
+        finalGrid = new short[grid.length/2][grid[0].length/2];
+
+        for (int r = 0; r < finalGrid.length; r++) {
+            for (int c = 0; c < finalGrid[r].length; c++) {
+                short avg = grid[r][c];
+                finalGrid[r][c] = avg;
+            }
+        }
+
+        img.setPixels(finalGrid);
         return img;
 
     }
